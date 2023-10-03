bedf33550c782d8a7fa279183c51842a519bb338
Maid Bajric <contact@baajraa.dev>
2023-10-03 13:26:03 +0200

    Fixes running scripts from inside monorepo workspace

--- run.js
+++ run.js
@@ -1,6 +1,15 @@
 const path = require('path');
+const os = require('os');
+
+const currentDir = path.resolve(process.cwd());
+const osTmpDir = path.resolve(os.tmpdir());
+
+if (currentDir.startsWith(osTmpDir)) {
+  return;
+}
+
 const script = require(path.resolve(__dirname, 'scripts', process.argv[2]));
 
 if (script instanceof Function) {
-  script(path.resolve(process.cwd(), '../typescript/bin/tsc'));
+  script(path.resolve(currentDir, '../typescript/bin/tsc'));
 }
